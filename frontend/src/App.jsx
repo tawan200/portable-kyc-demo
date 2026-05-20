f8cec45435ff257c5264228466ce06d6bdb23736
xiao <kkrt887@gmail.com>
2026-05-20 14:29:08 +0700

    set holder-wallet as default route and update holder wallet UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import HolderWallet from './views/holder/holder-wallet.jsx'
 import Issuer from './views/issuer/issuer.jsx'
 import Verifier from './views/verifier/verifier.jsx'
@@ -6,7 +6,7 @@ import Verifier from './views/verifier/verifier.jsx'
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/holder" replace />} />
+      <Route path="/" element={<HolderWallet />} />
       <Route path="/holder" element={<HolderWallet />} />
       <Route path="/issuer" element={<Issuer />} />
       <Route path="/verifier" element={<Verifier />} />
